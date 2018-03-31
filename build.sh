deff5de096d18f2490cf4cdaf49f058df7fc207a
Dan Sheffner <Dan@Sheffner.com>
2018-03-31 04:37:25 -0500

    faster builds

--- build.sh
+++ build.sh
@@ -3,12 +3,14 @@
 echo "build.sh started..."
 
 
-# lts
-time docker build -f Dockerfile-lts . -t thesheff17/docker_base:lts_latest
-time docker build -f Dockerfile-lts-gui . -t thesheff17/docker_base:lts_gui_latest
+# first set
+time docker build -f Dockerfile-lts . -t thesheff17/docker_base:lts_latest &
+time docker build -f Dockerfile-rolling . -t thesheff17/docker_base:rolling_latest &
+wait
 
-# rolling
-time docker build -f Dockerfile-rolling . -t thesheff17/docker_base:rolling_latest
-time docker build -f Dockerfile-rolling-gui . -t thesheff17/docker_base:rolling_gui_latest
+# second set
+time docker build -f Dockerfile-rolling-gui . -t thesheff17/docker_base:rolling_gui_latest &
+time docker build -f Dockerfile-lts-gui . -t thesheff17/docker_base:lts_gui_latest &
+wait
 
 echo "build.sh completed."
