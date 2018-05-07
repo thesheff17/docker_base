33431b449a67a4a8a14b3b2b016e4fd45845ed19
Dan Sheffner <Dan@Sheffner.com>
2018-05-07 06:47:04 -0500

    dynamic dates

--- build2.sh
+++ build2.sh
@@ -2,8 +2,8 @@
 
 echo "build.sh started..."
 
-time docker build -f Dockerfile-lts-gui . -t thesheff17/docker_base:lts_gui_latest-`date +"%m%d%Y"`
+time docker build -f Dockerfile-lts-gui CHANGEDATE=`date +"%m%d%Y"` . -t thesheff17/docker_base:lts_gui_latest-`date +"%m%d%Y"`
 time docker build -f Dockerfile-rolling . -t thesheff17/docker_base:rolling_latest-`date +"%m%d%Y"`
-time docker build -f Dockerfile-rolling-gui . -t thesheff17/docker_base:rolling_gui_latest-`date +"%m%d%Y"`
+time docker build -f Dockerfile-rolling-gui CHANGEDATE=`date +"%m%d%Y"` . -t thesheff17/docker_base:rolling_gui_latest-`date +"%m%d%Y"`
 
 echo "build.sh completed."
