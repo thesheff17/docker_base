48facf7281ed9837a9ca7d21f4cafdb412ed836c
Dan Sheffner <Dan@Sheffner.com>
2018-05-07 06:41:19 -0500

    build script

--- build2.sh
+++ build2.sh
@@ -2,7 +2,6 @@
 
 echo "build.sh started..."
 
-time docker build -f Dockerfile-lts . -t thesheff17/docker_base:lts_latest-`date +"%m%d%Y"`
 time docker build -f Dockerfile-lts-gui . -t thesheff17/docker_base:lts_gui_latest-`date +"%m%d%Y"`
 time docker build -f Dockerfile-rolling . -t thesheff17/docker_base:rolling_latest-`date +"%m%d%Y"`
 time docker build -f Dockerfile-rolling-gui . -t thesheff17/docker_base:rolling_gui_latest-`date +"%m%d%Y"`
